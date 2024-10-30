f5e66733c8b73366e81b381caa25a3ebb919d482
Phạm Tiến Việt <gdschanu.vietpt0905@gmail.com>
2024-10-30 10:37:56 +0700

    Update modal.vue

--- src/components/team/story/modal.vue
+++ src/components/team/story/modal.vue
@@ -2,8 +2,15 @@
   <div>
     <UButton :padded="false" variant="link" @click="isOpen = true">Read more</UButton>
     <UModal v-model="isOpen" fullscreen>
-      <div
-       class="h-screen"
+      <UCard
+        :ui="{
+          base: 'h-full flex flex-col',
+          rounded: '',
+          divide: 'divide-y divide-gray-100 dark:divide-gray-800',
+          body: {
+            base: 'grow'
+          }
+        }"
       >
         <template #header>
           <div class="flex items-center justify-between">
@@ -26,7 +33,7 @@
         <p class="mb-4">He then imagined a future of Vietnamese youth receiving those resources and having an incredible amount of innovation. But in reality, doing startups is often associated with uncertainties and failures, making this pathway "too risky" for Vietnamese youths to follow. Even when the trend of startups was rolling around, most entrepreneurial activities for students in Vietnam were pitching competitions. </p>
         <p class="mb-4">Students lean towards ideating quick-fix solutions and showcasing their business skills, while the whole process of validating problems, building products, and operating real startups is out of focus. Thereby, those competitions can neither give student entrepreneurs a <span class="text-primary-700">hands-on learning experience</span>, nor help them <span class="text-primary-700">hone in traits of a real founder. </span></p>
         <p>As a result, available resources in Vietnam are mostly targeting "mature founders", who are believed to have higher commitment to their startup journeys.</p>
-      </div>
+        </UCard>
     </UModal>
   </div>
 </template>
